d44ec57bf3f41d1a18c60966e2ba47c5dfa1187d
Tesla Tong <tongtianli@126.com>
2019-04-14 23:26:08 +0800

    MusicTableWidget complete, add save and load

--- absFiles/musictablewidget.cpp
+++ absFiles/musictablewidget.cpp
@@ -3,9 +3,9 @@
 MusicTableWidget::MusicTableWidget(QWidget *parent, QString tableName) :
     QTableWidget(parent)
 {
-    table = this;
     manager = new FileManager(tableName);
     name = tableName;
+    preventChangeSignal = false;
 }
 
 MusicTableWidget::~MusicTableWidget()
@@ -14,17 +14,153 @@ MusicTableWidget::~MusicTableWidget()
     delete this;
 }
 
-void MusicTableWidget::initColumnCats(QList<QString> categories)
+void MusicTableWidget::insertMusic(int index, Music *music)
 {
-    int n = categories.size();
-    QTableWidgetItem *headitem;
-    table->setColumnCount(n);
-    for(int i=0;i<categories.size();i++){
-        headitem = new QTableWidgetItem(categories.at(i));
-        QFont font = headitem->font();
-        font.setBold(true);
-        font.setPointSize(12);
-        headitem->setFont(font);
-        table->setHorizontalHeaderItem(i,headitem);
+    list.insert(index,music);
+    this->insertRow(index);
+    for(int i=0;i<this->columnCount();i++){
+        QString headerText = this->horizontalHeaderItem(i)->text();
+        if(headerText == "音乐标题"){
+            this->setItem(index,i,new QTableWidgetItem(music->name));
+            continue;
+        }
+        if(headerText == "歌手"){
+            this->setItem(index,i,new QTableWidgetItem(music->singer));
+            continue;
+        }
+        if(headerText == "时长"){
+            this->setItem(index,i,new QTableWidgetItem(music->duration));
+            continue;
+        }
+        if(headerText == "大小"){
+            this->setItem(index,i,new QTableWidgetItem(music->size));
+            continue;
+        }
+    }
+    if(!preventChangeSignal)
+        emit sizeChanged(list.size());
+}
+
+void MusicTableWidget::setMusiclist(QList<Music *> list)
+{
+    preventChangeSignal = true;
+    this->list=list;
+    foreach(Music* music, list){
+        append(music);
+    }
+    emit sizeChanged(list.size());
+    preventChangeSignal = false;
+}
+
+void MusicTableWidget::clearAll()
+{
+    preventChangeSignal = true;
+    while(rowCount()>0)
+        remove(0);
+    emit sizeChanged(list.size());
+    preventChangeSignal = false;
+}
+
+void MusicTableWidget::remove(int index)
+{
+    removeRow(index);
+    list.removeAt(index);
+    if(!preventChangeSignal)
+        emit sizeChanged(list.size());
+}
+
+void MusicTableWidget::append(Music *music)
+{
+    int size = list.size();
+    size = size<0?0:size;
+    insertMusic(size,music);
+}
+
+Music *MusicTableWidget::get(int index)
+{
+    return list[index];
+}
+
+QList<Music *> MusicTableWidget::getAll()
+{
+    return list;
+}
+
+void MusicTableWidget::load()
+{
+    QString fileDir = QDir::currentPath() +"/data/"+ name +".xml";
+    QFile file(fileDir);
+    if(not file.open(QFile::ReadOnly | QFile::Text))
+    {
+        qDebug()<<"No cache file:"<<fileDir;
+        return;
+    }
+    QXmlStreamReader reader(&file);
+    reader.readNextStartElement();
+    qDebug()<<reader.text();
+    if(reader.isStartElement()&&reader.name()!="musiclist")
+        return;
+    name = reader.attributes().at(0).value().toString();
+    reader.readNextStartElement();
+    Music *music = nullptr;
+    while(!reader.atEnd()){
+        if(reader.isStartElement()&&reader.name()=="music"){
+            if(music!=nullptr)
+                append(music);
+            music = new Music();
+            reader.readNext();
+        }
+        if(reader.name()=="id")
+            music->ID = reader.readElementText().toInt();
+        else if(reader.name()=="name")
+            music->name = reader.readElementText();
+        else if(reader.name()=="singer")
+            music->singer = reader.readElementText();
+        else if(reader.name()=="duration")
+            music->duration = reader.readElementText();
+        else if(reader.name()=="pic")
+            music->pic = reader.readElementText();
+        else if(reader.name()=="lrc")
+            music->lrc = reader.readElementText();
+        else if(reader.name()=="url")
+            music->url = reader.readElementText();
+        else if(reader.name()=="local")
+            music->local = reader.readElementText()=="true"?true:false;
+        else if(reader.name()=="size")
+            music->size = reader.readElementText();
+        reader.readNext();
     }
 }
+
+void MusicTableWidget::save()
+{
+    QString fileDir = QDir::currentPath() +"/data/"+ name +".xml";
+    QFile file(fileDir);
+    if(not file.open(QIODevice::ReadWrite|QIODevice::Truncate)){
+        qDebug()<<"can not open"<<fileDir<<endl;
+        return;
+    }
+    QXmlStreamWriter writer(&file);
+    writer.setAutoFormatting(true);
+    writer.writeStartDocument();
+    writer.writeStartElement("musiclist");
+    writer.writeAttribute("listname",name);
+    for(int i=0;i<list.count();i++){
+        Music *music = list[i];
+        writer.writeStartElement("music");
+        writer.writeTextElement("id",QString::number(music->ID,10));
+        writer.writeTextElement("name",music->name);
+        writer.writeTextElement("singer",music->singer);
+        writer.writeTextElement("duration",music->duration);
+        writer.writeTextElement("pic",music->pic.toString());
+        writer.writeTextElement("lrc",music->lrc.toString());
+        writer.writeTextElement("url",music->url.toString());
+        writer.writeTextElement("local",music->local?"true":"false");
+        writer.writeTextElement("size",music->size);
+        writer.writeEndElement();
+    }
+    writer.writeEndElement();
+    writer.writeEndDocument();
+    file.close();
+}
+
